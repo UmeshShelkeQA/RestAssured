6af1315f340813587a87b4fe099a3424732cccca
charukalaskar <charukalaskar116@gmail.com>
2024-01-15 23:00:46 +0530

    [Charu]: Added all methods of RestAssured

--- src/test/java/test/RegisterTest.java
+++ src/test/java/test/RegisterTest.java
@@ -3,19 +3,20 @@ package test;
 import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import static io.restassured.response.Response.*;
+
 public class RegisterTest {
 
-	@Test
+//	@Test
 	public void listUserAPI() {
-		RestAssured.baseURI = "https://reqres.in"; 
+		baseURI = "https://reqres.in"; 
 //		RestAssured.basePath = "/api/users?page=2";
 		
-		Response response = RestAssured.get("/api/users?page=2") ;
+		Response response = get("/api/users?page=2") ;
 		System.out.println(response.asString());
 		
 		response.then()
@@ -25,4 +26,51 @@ public class RegisterTest {
 					.contentType(ContentType.JSON);
 
 	}
+	
+	//@Test
+	public void postCreateAPI() {
+		baseURI = "https://reqres.in"; 	
+		Response Postresponse = given()
+	    .contentType(ContentType.JSON)
+	    .body("{\r\n"
+	    		+ "    \"name\": \"morpheus\",\r\n"
+	    		+ "    \"job\": \"leader\"\r\n"
+	    		+ "}")
+	.when()
+	.post("/api/users");
+		System.out.println(Postresponse.asString() );
+		Postresponse.then()
+	    .statusCode(201);
+		
+	}
+	
+	//@Test
+	public void putCreateAPI() 
+	{
+		baseURI = "https://reqres.in"; 
+		Response putResponse = given()
+	    .contentType(ContentType.JSON)
+	   .body("{\r\n"
+	   		+ "    \"name\": \"morpheus\",\r\n"
+	   		+ "    \"job\": \"zion resident\"\r\n"
+	   		+ "}")
+	 .when()
+	    .put("/api/users/2");
+		System.out.println(putResponse.getBody().asString());
+		putResponse.then()
+	    .statusCode(200);
+	}
+	
+	@Test
+	public void deleteCreateAPI() 
+	{
+		baseURI = "https://reqres.in"; 
+		when()
+	    .delete("/api/users/2")
+	.then()
+	    .statusCode(204);
+	    
+	}	
 }
+
+
